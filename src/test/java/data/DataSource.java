a0126e4ea67023aedb153445c63de575e4ca6d06
hjp <hejinpengcs@foxmail.com>
2021-03-27 19:10:56 +0800

    add data driven

---
+++ src/test/java/data/DataSource.java
@@ -0,0 +1,32 @@
+package data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataSource {
+    /**
+     * 根据测试方法的名称，来使用不同的数据源。
+     *
+     * @return 不同测试方法的数据源
+     */
+    public Map<String, Object[][]> dataSource() {
+        Map<String, Object[][]> dataMap = new HashMap<>();
+        Object[][] testGetCandlestick = new Object[][]{
+                {"BTC_USDT", "1m", 0, "public/get-candlestick"},
+                {"BTC_USDT", "5m", 0, "public/get-candlestick"},
+                {"BTC_USDT", "15m", 0, "public/get-candlestick"},
+                {"BTC_USDT", "30m", 0, "public/get-candlestick"},
+                {"BTC_USDT", "1h", 0, "public/get-candlestick"},
+                {"BTC_USDT", "4h", 0, "public/get-candlestick"},
+                {"BTC_USDT", "6h", 0, "public/get-candlestick"},
+                {"BTC_USDT", "12h", 0, "public/get-candlestick"},
+                {"BTC_USDT", "1D", 0, "public/get-candlestick"},
+                {"BTC_USDT", "7D", 0, "public/get-candlestick"},
+                {"BTC_USDT", "14D", 0, "public/get-candlestick"},
+                {"BTC_USDT", "1M", 0, "public/get-candlestick"}
+        };
+        dataMap.put("testGetCandlestick", testGetCandlestick);
+        return dataMap;
+    }
+
+}
--- src/test/java/rest/PublicGetCandlestickTest.java
+++ src/test/java/rest/PublicGetCandlestickTest.java
@@ -1,9 +1,15 @@
 package rest;
 
 import api.rest.PublicGetCandlestick;
+import data.DataSource;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class PublicGetCandlestickTest {
 
@@ -20,10 +26,21 @@ public class PublicGetCandlestickTest {
     public void tearDown() {
     }
 
-    @Test(description = "check candlestick right or not")
-    public void testGetCandlestick() {
-        publicGetCandlestick.getCandlestick("BTC_USDT", "5m")
+    @DataProvider
+    public Object[][] dataProvider(Method method) {
+        DataSource data = new DataSource();
+        Object[][] obj = data.dataSource().get(method.getName());
+        return obj;
+    }
+
+    @Test(dataProvider = "dataProvider", description = "check candlestick right or not")
+    public void testGetCandlestick(String instrumentName, String timeFrame, int code, String method) {
+        publicGetCandlestick.getCandlestick(instrumentName, timeFrame)
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("code", equalTo(code))
+                .body("method", equalTo(method))
+                .body("result.instrument_name", equalTo(instrumentName))
+                .body("result.interval", equalTo(timeFrame));
     }
 }
